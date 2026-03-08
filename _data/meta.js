d40287a46e4c29dce3be4c7bd3ae8c5201e25a43
Diuxx <nicolasmarmot@gmail.com>
2026-03-08 18:05:37 +0100

    feat(404): add custom 404 page with layout and content; update sitemap and Nginx config

--- _data/meta.js
+++ _data/meta.js
@@ -4,7 +4,7 @@ export default {
     title: 'Nadine - Psycho-praticienne Gestalt à votre écoute',
     description: 'Je suis une psycho-praticienne certifiée en Gestalt et je vous accompagne sur le chemin de la découverte de soi et de l\'épanouissement personnel. Séances en présentiel ou à distance, pour adultes, adolescents, couples et familles. Découvrez la Gestalt, mon approche, pour qui elle s\'adresse et comment se déroule l\'accompagnement.',
     lang: 'fr',
-    siteUrl: 'https://nadinevotrepsy.fr',
+    siteUrl: 'https://nadine-votre-psy.fr',
     author: 'Nadine Marmot',
   },
   hero: {
